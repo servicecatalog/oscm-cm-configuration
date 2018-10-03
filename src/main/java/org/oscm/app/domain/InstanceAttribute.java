c69ffbacb87b827243a9b39c8f665db867eff5ad
StawickaE <ewa.stawicka@ts.fujitsu.com>
2018-10-03 08:55:49 +0200

    oscm-cm-configuration

--- src/main/java/org/oscm/app/domain/InstanceAttribute.java
+++
@@ -1,8 +0,0 @@
-package org.oscm.app.domain;
-
-import javax.persistence.Entity;
-
-@Entity
-public class InstanceAttribute extends InstanceKeyValueHolder{
-
-}
--- src/main/java/org/oscm/app/repository/InstanceRepository.java
+++
@@ -1,7 +0,0 @@
-package org.oscm.app.repository;
-
-import org.oscm.app.domain.Instance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstanceRepository extends JpaRepository<Instance, Long>{
-}
